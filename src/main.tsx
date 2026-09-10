eb231aa1284e53aff169ae1e4ab411f8f04f04eb
rkolettu <rishab.kolettu@gmail.com>
2026-09-10 14:03:11 -0400

    Route compare hash to metro comparison view

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CompareView } from './components/CompareView.tsx'
+
+function Root() {
+  const [showCompare, setShowCompare] = useState(
+    () => window.location.hash === '#compare',
+  )
+
+  useEffect(() => {
+    const syncHash = () => setShowCompare(window.location.hash === '#compare')
+    window.addEventListener('hashchange', syncHash)
+    return () => window.removeEventListener('hashchange', syncHash)
+  }, [])
+
+  return showCompare ? <CompareView /> : <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
